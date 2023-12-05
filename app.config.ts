300d3ca399b1455932c614daf991aae4a7ed252a
yali <tangozyusufali@gmail.com>
2023-12-06 02:26:42 +0300

    style(ui): update gray, button and icons

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,14 @@
 export default defineAppConfig({
   ui: {
     primary: "black",
+    gray: "zinc",
+    button: {
+      default: {
+        color: "black",
+      },
+    },
+    icons: {
+      dynamic: true,
+    },
   },
 });
