d55c4b2157a5e9c6e4378be4d25a83ce30a12723
yali <tangozyusufali@gmail.com>
2023-12-06 15:32:39 +0300

    feat: remove set timeout from projects client

--- clients/projects.ts
+++ clients/projects.ts
@@ -3,7 +3,6 @@ import type { ProjectProps } from "~/types";
 class ProjectsClient {
   public async get(): Promise<ProjectProps[] | null> {
     try {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
       const { data } = await useFetch("/api/projects");
       return data.value as ProjectProps[];
     } catch {
